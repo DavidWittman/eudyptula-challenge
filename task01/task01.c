509134d774e9f3d13f97a7f53b72352453bb501b
David Wittman <dave@wittman.xyz>
2016-04-10 00:28:55 -0500

    Task 01

---
+++ task01/task01.c
@@ -0,0 +1,29 @@
+/*
+main.c
+Eudyptula Challenge: Task 01
+
+This is a kernel module which prints "Hello World!" to the kernel
+debug log when loaded.
+*/
+
+#define MODULE
+#define LINUX
+#define __KERNEL__
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("David Wittman");
+MODULE_DESCRIPTION("Hello World example");
+
+int init_module(void)
+{
+   printk(KERN_DEBUG "Hello World!\n");
+   return 0;
+}
+
+
+void cleanup_module(void)
+{
+}
