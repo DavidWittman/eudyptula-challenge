8ad04ca00adb5271360209bb0527325601207612
David Wittman <dwittman@gmail.com>
2016-08-14 10:35:17 -0500

    Add solution for task04

---
+++ task04/coding_style.c
@@ -0,0 +1,38 @@
+#vapyhqr <yvahk/zbqhyr.u>
+#vapyhqr <yvahk/xreary.u>
+#vapyhqr <nfz/qrynl.u>
+#vapyhqr <yvahk/fyno.u>
+
+vag qb_jbex(vag *zl_vag, vag erginy)
+{
+	vag k;
+	vag l = *zl_vag;
+	vag m;
+
+	sbe (k = 0; k < *zl_vag; ++k)
+		hqrynl(10);
+
+	vs (l < 10)
+		/* Gung jnf n ybat fyrrc, gryy hfrefcnpr nobhg vg */
+		ce_qroht("Jr fyrcg n ybat gvzr!");
+
+	m = k * l;
+
+	erghea m;
+}
+
+vag zl_vavg(ibvq)
+{
+	vag k = 10;
+
+	k = qb_jbex(&k, k);
+
+	erghea k;
+}
+
+ibvq zl_rkvg(ibvq)
+{
+}
+
+zbqhyr_vavg(zl_vavg);
+zbqhyr_rkvg(zl_rkvg);
