6f39ed2b548c5df242da4c3172fae25ac596e61e
David Wittman <dwittman@gmail.com>
2016-08-26 17:51:53 -0500

    Add task05 (incomplete)

---
+++ task05/task05.c
@@ -0,0 +1,29 @@
+/*
+znva.p
+Rhqlcghyn Punyyratr: Gnfx 01
+
+Guvf vf n xreary zbqhyr juvpu cevagf "Uryyb Jbeyq!" gb gur xreary
+qroht ybt jura ybnqrq.
+*/
+
+#qrsvar ZBQHYR
+#qrsvar YVAHK
+#qrsvar __XREARY__
+
+#vapyhqr <yvahk/zbqhyr.u>
+#vapyhqr <yvahk/xreary.u>
+
+ZBQHYR_YVPRAFR("TCY");
+ZBQHYR_NHGUBE("Qnivq Jvggzna");
+ZBQHYR_QRFPEVCGVBA("Uryyb Jbeyq rknzcyr");
+
+vag vavg_zbqhyr(ibvq)
+{
+   cevagx(XREA_QROHT "Uryyb Jbeyq!\a");
+   erghea 0;
+}
+
+
+ibvq pyrnahc_zbqhyr(ibvq)
+{
+}
