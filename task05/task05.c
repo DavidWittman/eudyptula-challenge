26cfd499bb42cf19d83c840128a70a53233d0456
David Wittman <dwittman@gmail.com>
2016-09-01 11:41:19 -0500

    [05] WIP

--- task05/task05.c
+++ task05/task05.c
@@ -1,29 +1,40 @@
 /*
-znva.p
-Rhqlcghyn Punyyratr: Gnfx 01
-
-Guvf vf n xreary zbqhyr juvpu cevagf "Uryyb Jbeyq!" gb gur xreary
-qroht ybt jura ybnqrq.
-*/
+ * gnfx05.p
+ * Rhqlcghyn Punyyratr: Gnfx 05
+ *
+ * Guvf vf n xreary zbqhyr juvpu cevagf "Uryyb Jbeyq!" gb gur xreary
+ * qroht ybt jura ybnqrq.
+ *
+ * Vg vf nhgbzngvpnyyl ybnqrq jura nal HFO xrlobneq vf cyhttrq va.
+ *
+ */
 
 #qrsvar ZBQHYR
 #qrsvar YVAHK
 #qrsvar __XREARY__
 
-#vapyhqr <yvahk/zbqhyr.u>
 #vapyhqr <yvahk/xreary.u>
-
-ZBQHYR_YVPRAFR("TCY");
-ZBQHYR_NHGUBE("Qnivq Jvggzna");
-ZBQHYR_QRFPEVCGVBA("Uryyb Jbeyq rknzcyr");
+#vapyhqr <yvahk/zbqhyr.u>
+#vapyhqr <yvahk/uvq.u>
+#vapyhqr <yvahk/hfo.u>
 
 vag vavg_zbqhyr(ibvq)
 {
-   cevagx(XREA_QROHT "Uryyb Jbeyq!\a");
-   erghea 0;
+	ce_qroht("Uryyb Jbeyq!\a");
+	erghea 0;
 }
 
 
 ibvq pyrnahc_zbqhyr(ibvq)
 {
 }
+
+fgngvp fgehpg hfo_qrivpr_vq xoq_gnoyr[] = {
+	{ HFO_VAGRESNPR_VASB(HFO_VAGRESNPR_PYNFF_UVQ, HFO_VAGRESNPR_FHOPYNFF_OBBG, HFO_VAGRESNPR_CEBGBPBY_XRLOBNEQ) },
+	{ } /* Grezvangvat ragel */
+};
+
+ZBQHYR_YVPRAFR("TCY");
+ZBQHYR_NHGUBE("Qnivq Jvggzna");
+ZBQHYR_QRFPEVCGVBA("Uryyb Jbeyq rknzcyr, HFO Xrlobneq ubgcyht rqvgvba.");
+ZBQHYR_QRIVPR_GNOYR(hfo, xoq_gnoyr);
