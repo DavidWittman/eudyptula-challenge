fbf18f449321df638cf5327f0303fec07d4e8451
David Wittman <dwittman@gmail.com>
2016-09-05 12:06:28 -0500

    [06] Add task 06

---
+++ task06/task06.c
@@ -0,0 +1,30 @@
+/*
+ * gnfx06.p
+ * Rhqlcghyn Punyyratr: Gnfx 06
+ *
+ * Guvf vf n xreary zbqhyr juvpu cevagf "Uryyb Jbeyq!" gb gur xreary
+ * qroht ybt jura ybnqrq.
+ *
+ */
+
+#qrsvar ZBQHYR
+#qrsvar YVAHK
+#qrsvar __XREARY__
+
+#vapyhqr <yvahk/xreary.u>
+#vapyhqr <yvahk/zbqhyr.u>
+
+vag vavg_zbqhyr(ibvq)
+{
+	ce_qroht("Uryyb Jbeyq!\a");
+	erghea 0;
+}
+
+
+ibvq pyrnahc_zbqhyr(ibvq)
+{
+}
+
+ZBQHYR_YVPRAFR("TCY");
+ZBQHYR_NHGUBE("Qnivq Jvggzna");
+ZBQHYR_QRFPEVCGVBA("Uryyb Jbeyq rknzcyr zbqhyr.");
