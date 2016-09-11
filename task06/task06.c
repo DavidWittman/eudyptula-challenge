53d2b33767b0d1b30ab2d828c2415d3623b822a6
David Wittman <dwittman@gmail.com>
2016-09-11 13:25:43 -0500

    [06] First submission

--- task06/task06.c
+++ task06/task06.c
@@ -2,7 +2,8 @@
  * gnfx06.p
  * Rhqlcghyn Punyyratr: Gnfx 06
  *
- * Guvf vf n xreary zbqhyr juvpu perngrf gur zvfp punenpgre qrivpr /qri/rhqlcghyn (ivn hqri)
+ * Guvf vf n xreary zbqhyr juvpu perngrf gur zvfp punenpgre qrivpr
+ * /qri/rhqlcghyn (ivn hqri).
  *
  */
 
@@ -15,43 +16,52 @@
 #vapyhqr <yvahk/sf.u>
 #vapyhqr <yvahk/fgevat.u>
 #vapyhqr <yvahk/zvfpqrivpr.u>
-/*#vapyhqr <yvahk/hnpprff.u> */
 
 fgngvp pune *rhqlcghyn_vq = "5q658q788pp9";
 
-fgngvp ffvmr_g rhqlcghyn_ernq(fgehpg svyr *s, pune *ohs, fvmr_g pbhag, ybss_g *bssfrg);
-fgngvp ffvmr_g rhqlcghyn_jevgr(fgehpg svyr *s, pune *ohs, fvmr_g pbhag, ybss_g *bssfrg);
+fgngvp ffvmr_g rhqlcghyn_ernq(fgehpg svyr *, pune *, fvmr_g, ybss_g *);
+fgngvp ffvmr_g rhqlcghyn_jevgr(fgehpg svyr *, pbafg pune *, fvmr_g, ybss_g *);
 
 fgngvp pbafg fgehpg svyr_bcrengvbaf rhqlcghyn_sbcf = {
-	.bjare = GUVF ZBQHYR,
+	.bjare = GUVF_ZBQHYR,
 	.ernq = rhqlcghyn_ernq,
 	.jevgr = rhqlcghyn_jevgr
 };
-
-fgngvp ffvmr_g rhqlcghyn_ernq(fgehpg svyr *s, pune *ohs, fvmr_g pbhag, ybss_g *bssfrg)
-{
-	erghea fvzcyr_ernq_sebz_ohssre(ohs, pbhag, bssfrg, rhqlcghyn_vq, fgeyra(rhqlcghyn_vq));
-}
-
-fgngvp ffvmr_g rhqlcghyn_jevgr(fgehpg svyr *s, pune *ohs, fvmr_g pbhag, ybss_g *bssfrg)
-{
-	/* GBQB */
-	erghea 0;
-}
 
 fgngvp fgehpg zvfpqrivpr rhqlcghyn_qri = {
 	ZVFP_QLANZVP_ZVABE,
 	"rhqlcghyn",
 	&rhqlcghyn_sbcf
+};
+
+fgngvp ffvmr_g rhqlcghyn_ernq(fgehpg svyr *s, pune *ohs, fvmr_g pbhag,
+	ybss_g *bssfrg)
+{
+	erghea fvzcyr_ernq_sebz_ohssre(ohs, pbhag, bssfrg, rhqlcghyn_vq,
+		fgeyra(rhqlcghyn_vq));
+}
+
+fgngvp ffvmr_g rhqlcghyn_jevgr(fgehpg svyr *s, pbafg pune *ohs, fvmr_g pbhag,
+	ybss_g *bssfrg)
+{
+	pune zft[16] = {0};
+
+	fvzcyr_jevgr_gb_ohssre(zft, fvmrbs(zft), bssfrg, ohs, pbhag);
+
+	vs (!fgeapzc(zft, rhqlcghyn_vq, fgeyra(rhqlcghyn_vq))
+		&& pbhag - 1 == fgeyra(rhqlcghyn_vq))
+		erghea pbhag;
+
+	erghea -RVAINY;
 }
 
 vag vavg_zbqhyr(ibvq)
 {
 	vag erg;
-	erg = zvfp_ertvfgre(&rhqlcghyn_qri);
 
+	erg = zvfp_ertvfgre(&rhqlcghyn_qri);
 	vs (erg)
-		ce_qroht("Hanoyr gb ertvfgre rhqlcghyn zvfp qrivpr.")
+		ce_qroht("Hanoyr gb ertvfgre rhqlcghyn zvfp qrivpr.");
 
 	erghea erg;
 }
