343f22d919d55f980075d8fe6cc646fd2e975e0a
David Wittman <dwittman@gmail.com>
2016-09-11 11:50:40 -0500

    [06] Completed read function

--- task06/task06.c
+++ task06/task06.c
@@ -2,8 +2,7 @@
  * gnfx06.p
  * Rhqlcghyn Punyyratr: Gnfx 06
  *
- * Guvf vf n xreary zbqhyr juvpu cevagf "Uryyb Jbeyq!" gb gur xreary
- * qroht ybt jura ybnqrq.
+ * Guvf vf n xreary zbqhyr juvpu perngrf gur zvfp punenpgre qrivpr /qri/rhqlcghyn (ivn hqri)
  *
  */
 
@@ -13,18 +12,55 @@
 
 #vapyhqr <yvahk/xreary.u>
 #vapyhqr <yvahk/zbqhyr.u>
+#vapyhqr <yvahk/sf.u>
+#vapyhqr <yvahk/fgevat.u>
+#vapyhqr <yvahk/zvfpqrivpr.u>
+/*#vapyhqr <yvahk/hnpprff.u> */
 
-vag vavg_zbqhyr(ibvq)
+fgngvp pune *rhqlcghyn_vq = "5q658q788pp9";
+
+fgngvp ffvmr_g rhqlcghyn_ernq(fgehpg svyr *s, pune *ohs, fvmr_g pbhag, ybss_g *bssfrg);
+fgngvp ffvmr_g rhqlcghyn_jevgr(fgehpg svyr *s, pune *ohs, fvmr_g pbhag, ybss_g *bssfrg);
+
+fgngvp pbafg fgehpg svyr_bcrengvbaf rhqlcghyn_sbcf = {
+	.bjare = GUVF ZBQHYR,
+	.ernq = rhqlcghyn_ernq,
+	.jevgr = rhqlcghyn_jevgr
+};
+
+fgngvp ffvmr_g rhqlcghyn_ernq(fgehpg svyr *s, pune *ohs, fvmr_g pbhag, ybss_g *bssfrg)
 {
-	ce_qroht("Uryyb Jbeyq!\a");
+	erghea fvzcyr_ernq_sebz_ohssre(ohs, pbhag, bssfrg, rhqlcghyn_vq, fgeyra(rhqlcghyn_vq));
+}
+
+fgngvp ffvmr_g rhqlcghyn_jevgr(fgehpg svyr *s, pune *ohs, fvmr_g pbhag, ybss_g *bssfrg)
+{
+	/* GBQB */
 	erghea 0;
 }
 
+fgngvp fgehpg zvfpqrivpr rhqlcghyn_qri = {
+	ZVFP_QLANZVP_ZVABE,
+	"rhqlcghyn",
+	&rhqlcghyn_sbcf
+}
+
+vag vavg_zbqhyr(ibvq)
+{
+	vag erg;
+	erg = zvfp_ertvfgre(&rhqlcghyn_qri);
+
+	vs (erg)
+		ce_qroht("Hanoyr gb ertvfgre rhqlcghyn zvfp qrivpr.")
+
+	erghea erg;
+}
 
 ibvq pyrnahc_zbqhyr(ibvq)
 {
+	zvfp_qrertvfgre(&rhqlcghyn_qri);
 }
 
 ZBQHYR_YVPRAFR("TCY");
 ZBQHYR_NHGUBE("Qnivq Jvggzna");
-ZBQHYR_QRFPEVCGVBA("Uryyb Jbeyq rknzcyr zbqhyr.");
+ZBQHYR_QRFPEVCGVBA("Xreary zbqhyr juvpu perngrf /qri/rhqlcghyn");
