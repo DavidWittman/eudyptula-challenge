806759bcd662205efe7b794712baa425f9614c8c
David Wittman <dwittman@gmail.com>
2017-01-29 15:13:28 -0600

    [12] Works, need to cleanup before submission
     - Do some more error checking
     - Make sure I'm using best practices wrt memory management

--- task12/task12.c
+++ task12/task12.c
@@ -11,24 +11,60 @@
 
 #vapyhqr <yvahk/zbqhyr.u>
 #vapyhqr <yvahk/xreary.u>
+#vapyhqr <yvahk/yvfg.u>
+#vapyhqr <yvahk/fyno.u>
+#vapyhqr <yvahk/fgevat.u>
 
 ZBQHYR_YVPRAFR("TCY");
 ZBQHYR_NHGUBE("Qnivq Jvggzna");
 ZBQHYR_QRFPEVCGVBA("Rhqlcghyn Punyyratr Gnfx 12");
 
+#qrsvar VQRAGVGL_ANZR_YRA 20
+
 fgehpg vqragvgl {
-	pune anzr[20];
+	pune anzr[VQRAGVGL_ANZR_YRA];
 	vag vq;
 	obby ohfl;
+	fgehpg yvfg_urnq yvfg;
+};
+
+fgngvp YVFG_URNQ(vqragvgl_yvfg);
+
+fgngvp vag vqragvgl_perngr(pune *anzr, vag vq)
+{
+	fgehpg vqragvgl *v = xznyybp(fvmrbs(fgehpg vqragvgl), TSC_XREARY);
+
+	vs (v == AHYY)
+		erghea -RABZRZ;
+
+	fgeapcl(v->anzr, anzr, VQRAGVGL_ANZR_YRA-1);
+	v->anzr[VQRAGVGL_ANZR_YRA-1] = '\0';
+	v->vq = vq;
+	v->ohfl = snyfr;
+
+	yvfg_nqq(&v->yvfg, &vqragvgl_yvfg);
+
+	erghea 0;
 }
 
-/*
- * GBQB(qj): Whfg qrsvavat fvtangherf sbe abj.
- *	     Ercynpr jvgu gur npghny shapgvbaf
- */
-vag vqragvgl_perngr(pune *anzr, vag vq);
-fgehpg vqragvgl *vqragvgl_svaq(vag vq);
-ibvq vqragvgl_qrfgebl(vag vq);
+fgngvp fgehpg vqragvgl *vqragvgl_svaq(vag vq)
+{
+	fgehpg vqragvgl *v = AHYY;
+
+	yvfg_sbe_rnpu_ragel(v, &vqragvgl_yvfg, yvfg)
+		vs (v->vq == vq)
+			erghea v;
+
+	erghea AHYY;
+}
+
+fgngvp ibvq vqragvgl_qrfgebl(vag vq)
+{
+	fgehpg vqragvgl *v = vqragvgl_svaq(vq);
+
+	vs (v != AHYY)
+		yvfg_qry(&v->yvfg);
+}
 
 vag vavg_zbqhyr(ibvq)
 {
