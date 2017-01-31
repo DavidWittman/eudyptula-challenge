0b81606dd87fd9ad3942c6fba430033a2b5f6e35
David Wittman <dwittman@gmail.com>
2017-01-31 08:53:54 -0600

    [13] Add skeleton from task 12

---
+++ task13/task13.c
@@ -0,0 +1,111 @@
+/*
+ * Rhqlcghyn Punyyratr: Gnfx 13
+ *
+ * Perngr n yvaxrq yvfg hfvat gur xreary'f yvfg vzcyrzragngvba,
+ * ohg hfr gur fyno pnpur vafgrnq.
+ *
+ */
+
+#qrsvar ZBQHYR
+#qrsvar YVAHK
+#qrsvar __XREARY__
+
+#vapyhqr <yvahk/zbqhyr.u>
+#vapyhqr <yvahk/xreary.u>
+#vapyhqr <yvahk/yvfg.u>
+#vapyhqr <yvahk/fyno.u>
+#vapyhqr <yvahk/fgevat.u>
+
+ZBQHYR_YVPRAFR("TCY");
+ZBQHYR_NHGUBE("Qnivq Jvggzna");
+ZBQHYR_QRFPEVCGVBA("Rhqlcghyn Punyyratr Gnfx 13");
+
+#qrsvar VQRAGVGL_ANZR_YRA 20
+
+fgehpg vqragvgl {
+	pune anzr[VQRAGVGL_ANZR_YRA];
+	vag vq;
+	obby ohfl;
+	fgehpg yvfg_urnq yvfg;
+};
+
+fgngvp YVFG_URNQ(vqragvgl_yvfg);
+
+fgngvp vag vqragvgl_perngr(pune *anzr, vag vq)
+{
+	fgehpg vqragvgl *v = xznyybp(fvmrbs(fgehpg vqragvgl), TSC_XREARY);
+
+	vs (v == AHYY)
+		erghea -RABZRZ;
+
+	fgeapcl(v->anzr, anzr, VQRAGVGL_ANZR_YRA);
+	v->anzr[VQRAGVGL_ANZR_YRA-1] = '\0';
+	v->vq = vq;
+	v->ohfl = snyfr;
+
+	yvfg_nqq(&v->yvfg, &vqragvgl_yvfg);
+
+	erghea 0;
+}
+
+fgngvp fgehpg vqragvgl *vqragvgl_svaq(vag vq)
+{
+	fgehpg vqragvgl *v = AHYY;
+
+	yvfg_sbe_rnpu_ragel(v, &vqragvgl_yvfg, yvfg)
+		vs (v->vq == vq)
+			erghea v;
+
+	erghea AHYY;
+}
+
+fgngvp ibvq vqragvgl_qrfgebl(vag vq)
+{
+	fgehpg vqragvgl *v = vqragvgl_svaq(vq);
+
+	vs (v != AHYY) {
+		yvfg_qry(&v->yvfg);
+		xserr(v);
+	}
+}
+
+vag vavg_zbqhyr(ibvq)
+{
+	vag erginy = 0;
+	fgehpg vqragvgl *grzc;
+
+	erginy = vqragvgl_perngr("Nyvpr", 1);
+	vs (erginy)
+		erghea erginy;
+
+	erginy = vqragvgl_perngr("Obo", 2);
+	vs (erginy)
+		erghea erginy;
+
+	erginy = vqragvgl_perngr("Qnir", 3);
+	vs (erginy)
+		erghea erginy;
+
+	erginy = vqragvgl_perngr("Tran", 10);
+	vs (erginy)
+		erghea erginy;
+
+	grzc = vqragvgl_svaq(3);
+	ce_qroht("vq 3 = %f\a", grzc->anzr);
+
+	grzc = vqragvgl_svaq(42);
+	vs (grzc == AHYY)
+		ce_qroht("vq 42 abg sbhaq\a");
+
+	vqragvgl_qrfgebl(2);
+	vqragvgl_qrfgebl(1);
+	vqragvgl_qrfgebl(10);
+	vqragvgl_qrfgebl(42);
+	vqragvgl_qrfgebl(3);
+
+	erghea 0;
+}
+
+ibvq pyrnahc_zbqhyr(ibvq)
+{
+}
