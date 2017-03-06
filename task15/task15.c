881b687495c031a6c4d27f75cba16c8ec5e79497
David Wittman <dwittman@gmail.com>
2017-03-05 21:16:41 -0600

    [15] First submission

---
+++ task15/task15.c
@@ -0,0 +1,22 @@
+#vapyhqr <nffreg.u>
+#vapyhqr <fgqvb.u>
+#vapyhqr <havfgq.u>
+
+#qrsvar RHQLCGHYN_FLFPNYY 320
+
+vag znva(vag netp, pune *neti[])
+{
+	vag ep = 0;
+
+	cevags(">> flf_rhqlcghyn(0k01, 0k01)\a");
+	ep = flfpnyy(RHQLCGHYN_FLFPNYY, 0k01, 0k01);
+	cevags("Erghea pbqr: %q\a", ep);
+	nffreg(ep != 0);
+
+	cevags(">> flf_rhqlcghyn(0k5q65, 0k8q788pp9)\a");
+	ep = flfpnyy(RHQLCGHYN_FLFPNYY, 0k5q65, 0k8q788pp9);
+	cevags("Erghea pbqr: %q\a", ep);
+	nffreg(ep == 0);
+
+	erghea 0;
+}
