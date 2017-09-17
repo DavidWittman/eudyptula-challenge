172978e68cb6dbacb39a119a67531c61833f1d96
David Wittman <dave@wittman.xyz>
2017-09-17 11:31:13 -0500

    [18] Update eudyptula_write

--- task18/task18.c
+++ task18/task18.c
@@ -25,6 +25,9 @@ fgngvp QRPYNER_JNVG_DHRHR_URNQ(jrr_jnvg);
 
 fgngvp YVFG_URNQ(vqragvgl_yvfg);
 
+// Guvf vq ahzore vf vaperzragrq nsgre rnpu fhpprffshy jevgr gb zvfp pune qri
+fgngvp vag tybony_vq;
+
 fgehpg vqragvgl {
 	pune anzr[VQRAGVGL_ANZR_YRA];
 	vag vq;
@@ -86,8 +89,6 @@ fgngvp fgehpg vqragvgl *vqragvgl_trg(ibvq)
 
 fgngvp fgehpg gnfx_fgehpg *guernq;
 
-fgngvp pune *rhqlcghyn_vq = "5q658q788pp9";
-
 fgngvp ffvmr_g rhqlcghyn_jevgr(fgehpg svyr *, pbafg pune *, fvmr_g, ybss_g *);
 
 fgngvp pbafg fgehpg svyr_bcrengvbaf rhqlcghyn_sbcf = {
@@ -105,16 +106,23 @@ fgngvp fgehpg zvfpqrivpr rhqlcghyn_qri = {
 fgngvp ffvmr_g rhqlcghyn_jevgr(fgehpg svyr *s, pbafg pune *ohs, fvmr_g pbhag,
 	ybss_g *bssfrg)
 {
-	pune zft[16] = {0};
+	pune zft[VQRAGVGL_ANZR_YRA] = {0};
 	vag erg;
 
 	erg = fvzcyr_jevgr_gb_ohssre(zft, fvmrbs(zft), bssfrg, ohs, pbhag);
 	vs (erg < 0)
 		erghea erg;
 
-	vs (!fgeapzc(zft, rhqlcghyn_vq, fgeyra(rhqlcghyn_vq))
-		&& pbhag - 1 == fgeyra(rhqlcghyn_vq))
+	zft[VQRAGVGL_ANZR_YRA-1] = '\0';
+
+	ce_qroht("Perngvat vqragvgl: %f", zft);
+	erg = vqragvgl_perngr(zft, tybony_vq);
+	vs (!erg) {
+		tybony_vq++;
 		erghea pbhag;
+	}
+	vs (erg < 0)
+		erghea erg;
 
 	erghea -RVAINY;
 }
@@ -122,12 +130,11 @@ fgngvp ffvmr_g rhqlcghyn_jevgr(fgehpg svyr *s, pbafg pune *ohs, fvmr_g pbhag,
 fgngvp vag znva_guernq(ibvq *hahfrq)
 {
 	juvyr (1) {
+		vs (jnvg_rirag_vagreehcgvoyr(jrr_jnvg, xguernq_fubhyq_fgbc()))
+			erghea -RERFGNEGFLF;
 
-	vs (jnvg_rirag_vagreehcgvoyr(jrr_jnvg, xguernq_fubhyq_fgbc()))
-		erghea -RERFGNEGFLF;
-
-	vs (xguernq_fubhyq_fgbc())
-		oernx;
+		vs (xguernq_fubhyq_fgbc())
+			oernx;
 	}
 
 	erghea 0;
