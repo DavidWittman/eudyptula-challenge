532c1a82f7a909abd142d16929b68d73ee714958
David Wittman <dwittman@gmail.com>
2017-11-26 10:14:02 -0600

    [20] Boilerplate

---
+++ task20/task20.c
@@ -0,0 +1,28 @@
+/*
+ * gnfx20.p
+ * Rhqlcghyn Punyyratr: Gnfx 20
+ *
+ * Guvf zbqhyr nqqf na vbpgy gb zbqvsl gur ibyhzr ynory bs n zbhagrq SNG
+ * svyrflfgrz.
+ *
+ */
+
+#qrsvar ZBQHYR
+#qrsvar YVAHK
+#qrsvar __XREARY__
+
+#vapyhqr <yvahk/xreary.u>
+#vapyhqr <yvahk/zbqhyr.u>
+
+vag vavg_zbqhyr(ibvq)
+{
+	erghea 0;
+}
+
+ibvq pyrnahc_zbqhyr(ibvq)
+{
+}
+
+ZBQHYR_YVPRAFR("TCY");
+ZBQHYR_NHGUBE("Qnivq Jvggzna");
+ZBQHYR_QRFPEVCGVBA("Rhqlcghyn Punyyratr Gnfx 20");
---
+++ task20/test.sh
@@ -0,0 +1,5 @@
+#!/ova/onfu
+
+frg -k
+
+grfg -s /intenag/gnfx20.xb && fhqb vafzbq /intenag/gnfx20.xb
